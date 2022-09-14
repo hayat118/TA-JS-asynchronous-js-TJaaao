4a888f8cc65eec256565dd3ddec9957e558a6cd4
ravi11o <ravi.suraj110@gmail.com>
2022-09-14 19:48:13 +0530

    image search

--- block-BJaaju/code/index.js
+++ block-BJaaju/code/index.js
@@ -1,14 +1,44 @@
-let input =document.querySelector("input");
+let searchElm =document.querySelector("input");
+let root=document.querySelector(".images");
+
+const url="https://api.unsplash.com/photos/?client_id=joptt4GPL8B2cH5kyPCvzaSOhyhG1MzowM76hVz2_DQ"
+
+const searchUrl=(query)=>
+`https://api.unsplash.com/search/photos?query=${query}&client_id=joptt4GPL8B2cH5kyPCvzaSOhyhG1MzowM76hVz2_DQ`
 
 
+function fetch(url,successHandler){
+  let xhr= new XMLHttpRequest();
+  xhr.open('GET',url);
+  xhr.onload=()=>successHandler(JSON.parse(xhr.response));
 
+  xhr.onerror=function(){
+    console.log(`Something went wrong`)
+  };
+  xhr.send();
+}
+
+function displayImages(images){
+  root.innerHTML=""
+ images.forEach(image=>{
+    let li=document.createElement("li");
+    let img=document.createElement("img");
+    img.src=image.urls.thumb;
+    li.append(img);
+    root.append(li);
+  })
+}
+
+fetch(url,displayImages);
 
 
 function handleInput(event){
-  if(event.keyCode===13 && input.value) {
-    let userName=input.value;
-    console.log(input.value)
+  if(event.keyCode===13 && searchElm.value) {
+    fetch(searchUrl(searchElm.value),(searchResult)=>{
+       displayImages(searchResult.results);
+    } );
+    searchElm.value=""
   }
 }
 
-input.addEventListener("keydown",handleInput)
+searchElm.addEventListener("keydown",handleInput);
